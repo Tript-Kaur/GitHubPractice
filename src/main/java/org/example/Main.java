908c106480354eee19aaf1eaa999fa9f3fd89f1f
Tript Kaur <triptkaur@gmail.com>
2023-02-08 13:05:47 +0530

    commit from command line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("Hello world!");
+        System.out.println("Hello ***************  world!");
         System.out.println("Stage 1:Initial");
         System.out.println("changes in feature local");
         System.out.println("Change made in Master Remote");
