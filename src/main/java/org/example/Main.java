8af24359d14cd76c40613b88165690d9b2c3a732
Tript-Kaur <98944316+Tript-Kaur@users.noreply.github.com>
2023-02-21 09:03:52 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
+    
     public static void main(String[] args) {
+        System.out.println("Hello Hello Hello   world!");
         System.out.println("Hello ***************  world!");
         System.out.println("Stage 1:Initial");
         System.out.println("changes in feature local");
